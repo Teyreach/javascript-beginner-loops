3e0da32495da5d0b49efa11db5d0ba6bc24b8ba9
Robert Koszela <dorso123@gmail.com>
2017-12-27 21:20:59 +0100

    zadanie9_4

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+function drawTree(h) {
+	for(var i = 1; i <= h; i++){
+		var star = "";
+		for(var j = 0; j<i; j++){
+			 star += '*';
+		}
+	console.log(star);
+	}	
+}
+
+var f2= drawTree(1);
+var f2= drawTree(2);
+var f3= drawTree(5);
+
+
+
